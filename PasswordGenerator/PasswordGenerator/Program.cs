0b5ef5fb72315425f1039ef36eda8572bb785b95
TimotheePopesco <timotheepopescoweb@gmail.com>
2023-04-25 16:02:52 +0200

    end of password generator project, import

--- PasswordGenerator/PasswordGenerator/Program.cs
+++ PasswordGenerator/PasswordGenerator/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using PassGenerator;
+using System;
+
+namespace PasswordGenerator
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            const int NB_PASSWORD = 10;
+
+            int lengthPassword = outils.AskPositifNumber("Longueur du mot de passe : ");
+
+
+            Console.WriteLine();
+            int choiceAlphabet = outils.AskNumberBetween("You want a password with :\n" +
+                "1 - Only lower case letters \n" +
+                "2 - Lower and upper case letters \n" +
+                "3 - Letters and numbers \n" +
+                "4 - Letters, numbers and special characters \n" +
+                "Your choice : ", 1, 4);
+
+            string minuscules = "abcdefghijklmnopqrstuvwxyz";
+            string majuscules = minuscules.ToUpper();
+            string numbers = "0123456789";
+            string specialCharacters = "#&+-!$?_";
+            string alphabet;
+            string password = "";
+            Random rand = new Random();
+
+            if (choiceAlphabet == 1)
+                alphabet = minuscules;
+            else if (choiceAlphabet == 2)
+                alphabet = minuscules + majuscules;
+            else if (choiceAlphabet == 3)
+                alphabet = minuscules + majuscules + numbers;
+            else
+                alphabet = minuscules + majuscules + numbers + specialCharacters;
+
+            int longeurAlphabet = alphabet.Length;
+
+            for (int j = 0; j < NB_PASSWORD; j++)
+            {
+                password = "";
+                for (int i = 0; i < lengthPassword; i++)
+                {
+                    int index = rand.Next(0, longeurAlphabet);
+                    password += alphabet[index];
+                }
+
+                Console.WriteLine("Password : " + password);
+            }
+        }
+    }
+}
---
+++ PasswordGenerator/PasswordGenerator/outils.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace PassGenerator
+{
+    static class outils
+    {
+        public static int AskPositifNumber(string question)
+        {
+            return AskNumberBetween(question, 1, int.MaxValue, "ERROR: The number must be positive and not zero");
+        }
+
+        public static int AskNumberBetween(string question, int min, int max, string messageErrorCustomized = null)
+        {
+            int number = AskNumber(question);
+            if ((number >= min) && (number <= max))
+            {
+                return number;
+            }
+            if (messageErrorCustomized == null)
+            {
+                Console.WriteLine("ERROR : The number must be between " + min + " and " + max);
+            }
+            else
+            {
+                Console.WriteLine(messageErrorCustomized);
+            }
+
+            Console.WriteLine();
+
+            return AskNumberBetween(question, min, max, messageErrorCustomized);
+        }
+
+        public static int AskNumber(string question)
+        {
+      
+            while (true)
+            {
+                Console.Write(question);
+                string answer = Console.ReadLine();
+                try
+                {
+                    int answerInt = int.Parse(answer);
+                    return answerInt;
+                }
+                catch
+                {
+                    Console.WriteLine("ERROR : You must enter a number");
+                    Console.WriteLine();
+                }
+            }
+        }
+
+    }
+}
