6892bf85d055e5837c69a4af53991a99ce06da7d
TimotheePopesco <timotheepopescoweb@gmail.com>
2023-04-25 14:39:39 +0200

    First import of my two first projects

---
+++ MathsGame/MathsGame/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace MathsGame
+{
+    class Program
+    {
+        enum e_Operator
+        {
+            ADDITION = 1,
+            MULTIPLICATION = 2,
+            SUBTRACTION = 3
+        }
+
+        static bool AskQuestion(int min, int max)
+        {
+            Random rand = new Random();
+            int answerInt = 0;
+
+            while (true)
+            {
+                int a = rand.Next(min, max + 1);
+                int b = rand.Next(min, max + 1);
+                e_Operator o = (e_Operator)rand.Next(1, 4);
+                int expectedResult;
+
+
+                switch (o)
+                {
+                    case e_Operator.ADDITION:
+                        Console.Write(a + " + " + b + " = ");
+                        expectedResult = a + b;
+                        break;
+                    case e_Operator.MULTIPLICATION:
+                        Console.Write(a + " x " + b + " = ");
+                        expectedResult = a * b;
+                        break;
+                    case e_Operator.SUBTRACTION:
+                        Console.Write(a + " - " + b + " = ");
+                        expectedResult = a - b;
+                        break;
+                    default:
+                    
+                        Console.WriteLine("ERROR : Unknown operator");
+                        return false;
+                }
+
+                string answer = Console.ReadLine();
+                try
+                {
+                    answerInt = int.Parse(answer);
+                    if (answerInt == expectedResult)
+                    {
+                        return true;
+                    }
+
+                    return false;
+                }
+                catch
+                {
+                    Console.WriteLine("ERROR : You must enter a number");
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+            const int MIN = -50;
+            const int MAX = 50;
+            const int NB_QUESTIONS = 5;
+
+            int points = 0;
+
+            for (int i = 0; i < NB_QUESTIONS; i++)
+            {
+                Console.WriteLine("Question n°" + (i + 1) + "/" + NB_QUESTIONS);
+                bool goodAnswer = AskQuestion(MIN, MAX);
+                if (goodAnswer)
+                {
+                    Console.WriteLine("Good answer");
+                    points++;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect answer !");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Your result : " + points + "/" + NB_QUESTIONS);
+
+            int moyenne = NB_QUESTIONS / 2;
+
+            if (points == NB_QUESTIONS)
+            {
+                Console.WriteLine("Excellent !");
+            }
+            else if (points == 0)
+            {
+                Console.WriteLine("You need to review !");
+            }
+            else if (points > moyenne)
+            {
+                Console.WriteLine("Not bad at all");
+            }
+            else
+            {
+                Console.WriteLine("Can do better");
+            }
+        }
+    }
+}
---
+++ SecretNumber/SecretNumber/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace SecretNumber
+{
+    class Program
+    {
+        // AskNumber
+        // display : Enter a number
+        // test if this number is valid (conversion -> try/catch) -> ERROR : this number is not valid
+        // loop as long as the number is not valid
+        // consider that 0 is invalid
+        // return the value (int)
+
+        static int AskNumber(int min, int max)
+        {
+            int userNumber = max + 1;
+
+            while ((userNumber < min) || (userNumber > max))
+            {
+                Console.Write("Enter a number between " + min + " and " + max + " : ");
+                string answer = Console.ReadLine();
+
+                try
+                {
+                    userNumber = int.Parse(answer);
+
+                    if ((userNumber < min) || (userNumber > max))
+                    {
+                        Console.WriteLine("ERROR : The number must be between " + min + " and " + max);
+                    }
+
+                }
+                catch
+                {
+                    Console.WriteLine("ERROR : Enter a valid number");
+                }
+            }
+
+            return userNumber;
+        }
+
+        static void Main(string[] args)
+        {
+            Random randint = new Random();
+
+            const int MIN = 1;
+            const int MAX = 10;
+            int secretNumber = randint.Next(MIN, MAX + 1);
+
+
+            int number = secretNumber + 1;
+
+            int lifes = 3;
+
+            while (lifes > 0) 
+            {
+                Console.WriteLine();
+                Console.WriteLine("Vies restantes : " + lifes);
+                number = AskNumber(MIN,MAX);
+
+                if (secretNumber > number)
+                {
+                    Console.WriteLine("Secret number is bigger");
+                }
+                else if (secretNumber < number)
+                {
+                    Console.WriteLine("Secret number is smaller");
+                }
+                else
+                {
+                    Console.WriteLine("Congrats, you find the secret number");
+                    break;
+                }
+                lifes--;
+            }
+
+            if (lifes == 0)
+            {
+                Console.WriteLine("You loose, the secret number was : " + secretNumber);
+            }
+
+        }
+    }
+}
